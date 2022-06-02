6bb8b9ebd61700179ed1e11ee957f4cd3dd8616a
Ivan F. Valerio <valerioif@gmail.com>
2022-06-02 00:26:23 -0400

    added configuration header file

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+#include "hmsConfig.hpp"
 
 auto main() -> int
 {
+  std::cout << "Welcome to HMS version "
+            << HMS_VERSION_MAJOR << "."
+            << HMS_VERSION_MINOR << "."
+            << HMS_VERSION_PATCH << "\n";
   return 0;
 }
